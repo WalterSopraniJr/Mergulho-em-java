fc2d41dd40a753142263ed1b26fde11bdab6328e
Walter Soprani Jr <91127730+WalterSopraniJr@users.noreply.github.com>
2022-04-06 23:28:34 -0300

    Add files via upload
    Testando sintaxes do Java para declaração de variáveis inteiras, decimais e analisando a resposta de uma operação "double".

---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,23 @@
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+		
+		double salario = 2500.50;
+		System.out.println("Meu salario é de R$" + salario);
+		
+		
+		double divisao = 3.14 / 2;
+		System.out.println(divisao);
+		
+		int divisaoNumeroInteiro = 5 / 2;
+		System.out.println(divisaoNumeroInteiro);
+		
+		double divisaoDecimal = 5 / 2;
+		System.out.println(divisaoDecimal);
+		
+		double outraDivisao = 5.0 / 2;
+		System.out.println(outraDivisao);
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,14 @@
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("Ola Dev");
+		
+		
+		int idade = 26;
+		
+		System.out.println("Sabemos que sua idade é de " + idade + " anos.");
+		
+	}
+
+}
