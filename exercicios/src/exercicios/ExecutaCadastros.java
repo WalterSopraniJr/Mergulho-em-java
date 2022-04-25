af20f887956cd1b17ada72eee4369925608f2a77
Walter Soprani Jr <91127730+WalterSopraniJr@users.noreply.github.com>
2022-04-25 06:25:16 -0300

    Utilizando as entradas de Pessoa de forma privada.

--- exercicios/src/exercicios/ExecutaCadastros.java
+++ exercicios/src/exercicios/ExecutaCadastros.java
@@ -4,10 +4,17 @@ public class ExecutaCadastros {
 	public static void main(String[] args) {
 		Cadastro walter = new Cadastro();
 		Pessoa walterPessoa = new Pessoa();
-		System.out.println("Endereço de memória de 'Cadastro': " + walter + " e de 'Pessoa': "+ walterPessoa);
-		walterPessoa.setNome("Walter Soprani Júnior");
-		System.out.println(walterPessoa.getNome());
 		
+		
+		walterPessoa.setNome("Walter Soprani Júnior");
+		walterPessoa.setCPF("222.222.222-22");
+		walterPessoa.setProfissao("Programador");
+		
+		//System.out.println("Endereço de memória de 'Cadastro': " + walter + " e de 'Pessoa': "+ walterPessoa);
+		
+		System.out.println("Nome: " + walterPessoa.getNome());
+		System.out.println("CPF: " + walterPessoa.getCPF());
+		System.out.println("Profissão: " + walterPessoa.getProfissao());
 	}
 
 }
--- exercicios/src/exercicios/Pessoa.java
+++ exercicios/src/exercicios/Pessoa.java
@@ -2,8 +2,8 @@ package exercicios;
 
 public class Pessoa {
 	private String nome;
-	String cpf;
-	String profissao;
+	private String cpf;
+	private String profissao;
 	
 	
 	public String getNome() {
@@ -11,5 +11,17 @@ public class Pessoa {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public String getCPF() {
+		return this.cpf;
+	}
+	public void setCPF(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
 	}
 }
