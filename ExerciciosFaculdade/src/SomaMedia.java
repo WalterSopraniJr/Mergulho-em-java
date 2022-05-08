f861d88016edbb23684610868c9679e1fb8443d9
Walter Soprani Jr <91127730+WalterSopraniJr@users.noreply.github.com>
2022-05-08 16:16:32 -0300

    Exercício de declaração de notas e calculo de med.

---
+++ ExerciciosFaculdade/src/SomaMedia.java
@@ -0,0 +1,26 @@
+
+public class SomaMedia {
+
+	public static void main(String[] args) {
+		String nomeAlgoritimo = "Soma e media";
+		String nomeAluno = "Walter Soprani Jr";
+		double provaUm = 7.5;
+		double provaDois = 8.5;
+		double provaTres = 9;
+		double provaQuatro = 9.5;
+		double provaCinco = 10;
+		double media = (provaUm + provaDois + provaTres + provaQuatro + provaCinco) / 5;
+		
+		System.out.println("Resultados das provas e média final");
+		System.out.println("Aluno: " + nomeAluno);
+		System.out.println("Nota prova um: " + provaUm);
+		System.out.println("Nota prova dois: " + provaDois);
+		System.out.println("Nota prova tres: " + provaTres);
+		System.out.println("Nota prova quatro: " + provaQuatro);
+		System.out.println("Nota prova cinco: " + provaCinco);
+		System.out.println("Média fina : " + media);
+		
+		
+	}
+
+}
